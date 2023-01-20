f043777d2b4844626618e01babcd444287680128
Basudev Tyagi <tyagi101basudev@gmail.com>
2023-01-21 02:06:15 +0530

    feat: add file stream logger

--- main.py
+++ main.py
@@ -5,28 +5,34 @@ import requests
 from scrapper import scrapper
 import schedule
 
+import logging
+logging.basicConfig(filename='main.log', filemode='w', format='[%(levelname)s] %(asctime)s | %(name)s.%(funcName)s - %(message)s')
 
 WEBHOOK_URL = "https://discord.com/api/webhooks/1066061125709463633/uiPa_3GYsU95hJtjMoq3zBSRsd25hY4nidcpx_s89vsQBXXhjoJWZl3GX4S7Bx6hYC5k"
 
 
 def send_webhook(data):
+    logging.info(f"Sending Webhook with title: {data.get('embeds', [{}])[0].get('title')}")
     try:
         response = requests.post(WEBHOOK_URL, headers={'Content-Type': 'application/json'}, data=json.dumps(data))
         if response.status_code == 200:
-            pass
+            logging.info(f"Webhook sent successfully with title: {data.get('embeds', [{}])[0].get('title')}")
         else:
-            pass
+            logging.error(f"Code [{response.status_code}]. Unable to send webhook: {response.content}")
     except Exception as error:
-        pass
+        logging.exception(f"ERROR: {error}")
 
 
 if __name__ == '__main__':
-    def schedule_job():
-        schedule.every().hour.do(scrapper, send_webhook)
-        return schedule.CancelJob
+    try:
+        def schedule_job():
+            schedule.every().hour.do(scrapper, send_webhook)
+            return schedule.CancelJob
 
-    schedule.every().day.at('09:00').do(schedule_job)
+        schedule.every().day.at('09:00').do(schedule_job)
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+    except Exception as error:
+        logging.critical(f"ERROR: {error}")
--- scrapper.py
+++ scrapper.py
@@ -2,6 +2,9 @@ from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 
+import logging
+logging.basicConfig(filename='scrapper.log', filemode='w', format='[%(levelname)s] %(asctime)s | %(name)s.%(funcName)s - %(message)s')
+
 BASE_URL = "https://csitgeu.in/wp/"
 
 
@@ -18,17 +21,23 @@ def scrapper(callback):
     date = get_current_date()
     last_notice_datetime = datetime.now().astimezone()
     try:
+        logging.info(f"Fetching notices dated: {date['year']}/{date['month']}/{date['day']}...")
         response = requests.get(f"{BASE_URL}{date['year']}/{date['month']}/{date['day']}")
+        logging.info(f"Response received for notices dated: {date['year']}/{date['month']}/{date['day']}...")
         soup = BeautifulSoup(response.content, 'html.parser')
         if response.status_code == 200:
             notices = soup.findAll('article')
+            logging.info(f"Fetched {len(notices)} notice(s)")
             for notice in notices:
                 timestamp = notice.find_next('span', class_='entry-date').find('time').get('datetime')
                 notice_datetime = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S%z')
+                logging.info(f"Last recent notice timestamp: {last_notice_datetime}")
+                logging.info(f"Current notice timestamp: {notice_datetime}")
                 if notice_datetime > last_notice_datetime:
                     last_notice_datetime = notice_datetime
                     title = notice.find_next('h1', class_='entry-title').text
                     url = notice.find_next('h1', class_='entry-title').find('a').get('href')
+                    logging.info("Calling send_webhook callback...")
                     callback({
                         "content": None,
                         "embeds": [
@@ -45,6 +54,6 @@ def scrapper(callback):
                         "attachments": []
                     })
         else:
-            pass
+            logging.warning("Unable to fetch notices. Reason: no notices for given date or incorrect request.")
     except Exception as error:
-        print(error)
+        logging.exception(f"ERROR: {error}")
